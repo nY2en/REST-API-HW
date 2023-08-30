ee99015b20c492af8fdb6f494b29372d2fe516a5
Nikita <chebanikita@icloud.com>
2023-08-30 17:25:21 +0300

    done main part of h/w

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const fs = require("fs/promises");
+const path = require("path");
+const gravatar = require("gravatar");
+const jimp = require("jimp");
 
 const { User } = require("../models/user");
 const { ctrlWrapper, HttpError } = require("../utils");
@@ -17,7 +21,13 @@ const register = async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ ...req.body, password: hashedPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashedPassword,
+    avatarURL,
+  });
   res.status(201).json({
     user: {
       email: newUser.email,
@@ -77,10 +87,34 @@ const updateSub = async (req, res) => {
   res.json({ subscription });
 };
 
+const updateAvatar = async (req, res) => {
+  if (!req.file) {
+    throw HttpError(400, "Bad Request");
+  }
+
+  const { _id } = req.user;
+  const { path: tempPath, originalname } = req.file;
+  const fileName = `${_id}_${originalname}`;
+  const avatarURL = path.join("avatars", fileName);
+  const newPath = path.join(__dirname, "../", "public", "avatars", fileName);
+
+  const picture = await jimp.read(tempPath);
+  picture.resize(250, 250).write(tempPath);
+
+  await fs.rename(tempPath, newPath);
+
+  await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
+
+  res.status(200).json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   logout: ctrlWrapper(logout),
   current: ctrlWrapper(current),
   updateSub: ctrlWrapper(updateSub),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
